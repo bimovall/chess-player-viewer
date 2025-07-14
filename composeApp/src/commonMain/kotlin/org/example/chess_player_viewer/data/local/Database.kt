e076b0da39bb6cd9e8d8a9a224f03f9a5fcb3137
bimovallentino <bimo_achmad@ymail.com>
2025-07-14 15:20:55 +0700

    chore: remove unused file

--- composeApp/src/commonMain/kotlin/org/example/chess_player_viewer/data/local/Database.kt
+++
@@ -1,9 +0,0 @@
-package org.example.chess_player_viewer.data.local
-
-import org.example.chess_player_viewer.ChessPlayerDB
-import org.example.chess_player_viewer.data.local.driver.DatabaseDriverFactory
-
-internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
-    private val driver = databaseDriverFactory.createDriver()
-    val database = ChessPlayerDB(driver)
-}
